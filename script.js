00c5cc4616bb8f55b03131744fb4ae72919f97a5
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-11-17 11:25:14 +0200

    11th Commit

--- script.js
+++ script.js
@@ -15,91 +15,112 @@ function pressCell(numberCell) {
 }
 
 function results() {
+    let is_winner = false;
     if (document.getElementById('cell1').innerHTML == 'X' 
         && document.getElementById('cell2').innerHTML == 'X'
         && document.getElementById('cell3').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (document.getElementById('cell1').innerHTML == '0' 
         && document.getElementById('cell2').innerHTML == '0'
         && document.getElementById('cell3').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell4').innerHTML == 'X' 
         && document.getElementById('cell5').innerHTML == 'X'
         && document.getElementById('cell6').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell4').innerHTML == '0' 
         && document.getElementById('cell5').innerHTML == '0'
         && document.getElementById('cell6').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell7').innerHTML == 'X' 
         && document.getElementById('cell8').innerHTML == 'X'
         && document.getElementById('cell9').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (document.getElementById('cell7').innerHTML == '0' 
         && document.getElementById('cell8').innerHTML == '0'
         && document.getElementById('cell9').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (document.getElementById('cell1').innerHTML == 'X' 
         && document.getElementById('cell4').innerHTML == 'X'
         && document.getElementById('cell7').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (document.getElementById('cell1').innerHTML == '0' 
         && document.getElementById('cell4').innerHTML == '0'
         && document.getElementById('cell7').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell2').innerHTML == 'X' 
         && document.getElementById('cell5').innerHTML == 'X'
         && document.getElementById('cell8').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell2').innerHTML == '0' 
         && document.getElementById('cell5').innerHTML == '0'
         && document.getElementById('cell8').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell3').innerHTML == 'X' 
         && document.getElementById('cell6').innerHTML == 'X'
         && document.getElementById('cell9').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (document.getElementById('cell3').innerHTML == '0' 
         && document.getElementById('cell6').innerHTML == '0'
         && document.getElementById('cell9').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (document.getElementById('cell1').innerHTML == 'X' 
         && document.getElementById('cell5').innerHTML == 'X'
         && document.getElementById('cell9').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (document.getElementById('cell1').innerHTML == '0' 
         && document.getElementById('cell5').innerHTML == '0'
         && document.getElementById('cell9').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell3').innerHTML == 'X' 
         && document.getElementById('cell5').innerHTML == 'X'
         && document.getElementById('cell7').innerHTML == 'X') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }  else if (document.getElementById('cell3').innerHTML == '0' 
         && document.getElementById('cell5').innerHTML == '0'
         && document.getElementById('cell7').innerHTML == '0') {
+            is_winner = true;
             document.getElementById('output').innerHTML = 'Winner!';
             document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     } else if (pressedCellCount == 10) {
         document.getElementById('output').innerHTML = 'Draw!';   
         document.getElementById('refresh').innerHTML = '<br><button class="btn btn-success" onclick="replay()">Play again</button>'
     }
-    
+    if (is_winner == true) {
+        for (let i = 1; i <= 9; ++i) {
+            document.getElementById('cell' + i + '').onclick = null;           
+        }
+    }
 }
 
 function replay() {
